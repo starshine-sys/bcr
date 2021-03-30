65494337c0b5731401bea25bf0ed0296bf851c03
Starshine System <us@starshines.xyz>
2021-03-30 13:26:02 +0200

    change PermStrings to use a custom slice type instead of maps

--- permCheck.go
+++ permCheck.go
@@ -16,44 +16,6 @@ func (p *PermError) Error() string {
 	return strings.Join(p.s, ", ")
 }
 
-// PermissionUseSlashCommands is the permission bit to use slash commands in a server
-const PermissionUseSlashCommands = 1 << 31
-
-var permNames = map[discord.Permissions]string{
-	discord.PermissionCreateInstantInvite: "Create Instant Invite",
-	discord.PermissionKickMembers:         "Kick Members",
-	discord.PermissionBanMembers:          "Ban Members",
-	discord.PermissionAdministrator:       "Administrator",
-	discord.PermissionManageChannels:      "Manage Channels",
-	discord.PermissionManageGuild:         "Manage Server",
-	discord.PermissionAddReactions:        "Add Reactions",
-	discord.PermissionViewAuditLog:        "View Audit Log",
-	discord.PermissionPrioritySpeaker:     "Priority Speaker",
-	discord.PermissionStream:              "Stream",
-	discord.PermissionViewChannel:         "View Channel",
-	discord.PermissionSendMessages:        "Send Messages",
-	discord.PermissionSendTTSMessages:     "Send TTS Messages",
-	discord.PermissionManageMessages:      "Manage Messages",
-	discord.PermissionEmbedLinks:          "Embed Links",
-	discord.PermissionAttachFiles:         "Attach Files",
-	discord.PermissionReadMessageHistory:  "Read Message History",
-	discord.PermissionMentionEveryone:     "Mention Everyone",
-	discord.PermissionUseExternalEmojis:   "Use External Emojis",
-	discord.PermissionConnect:             "Connect",
-	discord.PermissionSpeak:               "Speak",
-	discord.PermissionMuteMembers:         "Mute Members",
-	discord.PermissionDeafenMembers:       "Deafen Members",
-	discord.PermissionMoveMembers:         "Move Members",
-	discord.PermissionUseVAD:              "Use VAD",
-	discord.PermissionChangeNickname:      "Change Nickname",
-	discord.PermissionManageNicknames:     "Manage Nicknames",
-	discord.PermissionManageRoles:         "Manage Roles",
-	discord.PermissionManageWebhooks:      "Manage Webhooks",
-	discord.PermissionManageEmojis:        "Manage Emojis",
-	// This one isn't in arikawa or the developer docs yet, but *has* rolled out to roles
-	1 << 31: "Use Slash Commands",
-}
-
 // CheckPerms checks the user's permissions in the current channel
 func (ctx *Context) CheckPerms() (err error) {
 	return ctx.perms(ctx.Author.ID, ctx.Cmd.Permissions)
@@ -75,23 +37,6 @@ func (ctx *Context) perms(user discord.UserID, p discord.Permissions) (err error
 		return nil
 	}
 	return &PermError{s: PermStrings(p), perms: p}
-}
-
-// PermStrings gets the permission strings for all required permissions
-func PermStrings(p discord.Permissions) []string {
-	return PermStringsFor(permNames, p)
-}
-
-// PermStringsFor is like PermStrings but lets you specify the specific map used
-func PermStringsFor(m map[discord.Permissions]string, p discord.Permissions) []string {
-	var out = make([]string, 0, 32)
-	for perm, name := range m {
-		if p&perm == perm {
-			out = append(out, name)
-		}
-	}
-
-	return out
 }
 
 // checkOwner returns true if the user can run the command
---
+++ perm_strings.go
@@ -0,0 +1,89 @@
+package bcr
+
+import "github.com/diamondburned/arikawa/v2/discord"
+
+// Perm is a single permission
+type Perm struct {
+	Permission discord.Permissions
+	Name       string
+}
+
+// Permission constants that Arikawa is missing
+const (
+	PermissionViewServerInsights = 1 << 19
+	PermissionUseSlashCommands   = 1 << 31
+)
+
+// All permissions
+var (
+	MajorPerms = []Perm{
+		{discord.PermissionAdministrator, "Administrator"},
+		{discord.PermissionManageGuild, "Manage Server"},
+		{discord.PermissionManageWebhooks, "Manage Webhooks"},
+		{discord.PermissionManageChannels, "Manage Channels"},
+
+		{discord.PermissionBanMembers, "Ban Members"},
+		{discord.PermissionKickMembers, "Kick Members"},
+
+		{discord.PermissionManageRoles, "Manage Roles"},
+		{discord.PermissionManageNicknames, "Manage Nicknames"},
+		{discord.PermissionManageEmojis, "Manage Emojis"},
+		{discord.PermissionManageMessages, "Manage Messages"},
+
+		{discord.PermissionMentionEveryone, "Mention Everyone"},
+
+		{discord.PermissionMuteMembers, "Voice Mute Members"},
+		{discord.PermissionDeafenMembers, "Voice Deafen Members"},
+		{discord.PermissionMoveMembers, "Voice Move Members"},
+	}
+
+	NotablePerms = []Perm{
+		{discord.PermissionViewAuditLog, "View Audit Log"},
+		{PermissionViewServerInsights, "View Server Insights"},
+
+		{discord.PermissionPrioritySpeaker, "Priority Speaker"},
+		{discord.PermissionSendTTSMessages, "Send TTS Messages"},
+
+		{discord.PermissionCreateInstantInvite, "Create Invite"},
+	}
+
+	MinorPerms = []Perm{
+		{discord.PermissionStream, "Video"},
+		{discord.PermissionUseVAD, "Use Voice Activity"},
+		{discord.PermissionSpeak, "Speak"},
+		{discord.PermissionConnect, "Connect"},
+
+		{discord.PermissionAttachFiles, "Attach Files"},
+		{discord.PermissionEmbedLinks, "Embed Links"},
+
+		{discord.PermissionAddReactions, "Add Reactions"},
+		{discord.PermissionSendMessages, "Send Messages"},
+
+		{discord.PermissionReadMessageHistory, "Read Message History"},
+		{discord.PermissionViewChannel, "View Channel"},
+
+		{PermissionUseSlashCommands, "Use Slash Commands"},
+
+		{discord.PermissionChangeNickname, "Change Nickname"},
+		{discord.PermissionUseExternalEmojis, "Use External Emojis"},
+	}
+
+	AllPerms = append(MajorPerms, append(NotablePerms, MinorPerms...)...)
+)
+
+// PermStrings gives permission strings for all Discord permissions
+func PermStrings(p discord.Permissions) []string {
+	return PermStringsFor(AllPerms, p)
+}
+
+// PermStringsFor gives permission strings for the given Perm slice
+func PermStringsFor(m []Perm, p discord.Permissions) []string {
+	var out = make([]string, 0, 32)
+	for _, perm := range m {
+		if p&perm.Permission == perm.Permission {
+			out = append(out, perm.Name)
+		}
+	}
+
+	return out
+}
