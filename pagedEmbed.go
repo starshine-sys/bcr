b38493c26dc4585e0bf38a8a0bba801106918a56
Starshine System <us@starshines.xyz>
2021-04-24 13:00:13 +0200

    add field paginator

--- pagedEmbed.go
+++ pagedEmbed.go
@@ -2,6 +2,8 @@ package bcr
 
 import (
 	"errors"
+	"fmt"
+	"math"
 
 	"github.com/diamondburned/arikawa/v2/discord"
 )
@@ -104,4 +106,46 @@ func (ctx *Context) PagedEmbed(embeds []discord.Embed, extendedReactions bool) (
 		})
 	}
 	return
+}
+
+// FieldPaginator paginates embed fields, for use in ctx.PagedEmbed
+func FieldPaginator(title, description string, colour discord.Color, fields []discord.EmbedField, perPage int) []discord.Embed {
+	var (
+		embeds []discord.Embed
+		count  int
+
+		pages = 1
+		buf   = discord.Embed{
+			Title:       title,
+			Description: description,
+			Color:       colour,
+			Footer: &discord.EmbedFooter{
+				Text: fmt.Sprintf("Page 1/%v", math.Ceil(float64(len(fields))/float64(perPage))),
+			},
+		}
+	)
+
+	for _, field := range fields {
+		if count >= perPage {
+			embeds = append(embeds, buf)
+			buf = discord.Embed{
+				Title:       title,
+				Description: description,
+				Color:       colour,
+				Footer: &discord.EmbedFooter{
+					Text: fmt.Sprintf("Page %v/%v", pages+1, math.Ceil(float64(len(fields))/float64(perPage))),
+				},
+			}
+			count = 0
+			pages++
+		}
+		buf.Fields = append(buf.Fields, field)
+		count++
+	}
+
+	if len(buf.Fields) > 0 {
+		embeds = append(embeds, buf)
+	}
+
+	return embeds
 }
